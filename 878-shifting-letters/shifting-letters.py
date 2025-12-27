5ababb962cdbeaed12d1ca9d6f90fe898cd8cfdc
MONISH S <monish350s@gmail.com>
2025-12-27 14:59:12 +0530

    Time: 123 ms (86.67%) | Memory: 19.9 MB (96.67%) - LeetSync

---
+++ 878-shifting-letters/shifting-letters.py
@@ -0,0 +1,8 @@
+class Solution(object):
+    def shiftingLetters(self, s, shifts):
+        ans, shift = '', 0
+        for i in range(len(shifts) -1, -1, -1):
+            ans += chr((ord(s[i]) - ord('a') + shift+shifts[i]) % 26 + ord('a'))
+            shift += shifts[i]
+        
+        return ans[::-1]
